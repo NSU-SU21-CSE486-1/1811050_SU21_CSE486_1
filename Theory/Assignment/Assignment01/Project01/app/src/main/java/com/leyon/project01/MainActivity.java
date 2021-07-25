bf59a15b511e6270de859a0ad1ddb773371b5c95
LeyonIbnKamal <leyon.kamal@northsouth.edu>
2021-07-25 23:55:46 +0600

    #2 - Implemented signup form data collection pages and ability to cycle through the pages in one activity.

--- Theory/Assignment/Assignment01/Project01/app/src/main/java/com/leyon/project01/MainActivity.java
+++ Theory/Assignment/Assignment01/Project01/app/src/main/java/com/leyon/project01/MainActivity.java
@@ -4,19 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    int signUpPageNo = 1;
-    int maxPages = 3;
+    int currentSignUpPageNo = 1;
+    int maxPages = 4;
+
+    LinearLayout signUpPage1;
+    LinearLayout signUpPage2;
+    LinearLayout signUpPage3;
+    LinearLayout signUpPage4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpPage1 = findViewById(R.id.signUpPage1);
+        signUpPage2 = findViewById(R.id.signUpPage2);
+        signUpPage3 = findViewById(R.id.signUpPage3);
+        signUpPage4 = findViewById(R.id.signUpPage4);
     }
 
     public void GoToNext(View view) {
         //switch pages by changing visibility until visit last page. then submit data.
+        currentSignUpPageNo++;
+
+        switch (currentSignUpPageNo) {
+            case 1:
+                signUpPage1.setVisibility(View.VISIBLE);
+                signUpPage2.setVisibility(View.GONE);
+                signUpPage3.setVisibility(View.GONE);
+                signUpPage4.setVisibility(View.GONE);
+                break;
+            case 2:
+                signUpPage1.setVisibility(View.GONE);
+                signUpPage2.setVisibility(View.VISIBLE);
+                signUpPage3.setVisibility(View.GONE);
+                signUpPage4.setVisibility(View.GONE);
+                break;
+            case 3:
+                signUpPage1.setVisibility(View.GONE);
+                signUpPage2.setVisibility(View.GONE);
+                signUpPage3.setVisibility(View.VISIBLE);
+                signUpPage4.setVisibility(View.GONE);
+                break;
+            case 4:
+                signUpPage1.setVisibility(View.GONE);
+                signUpPage2.setVisibility(View.GONE);
+                signUpPage3.setVisibility(View.GONE);
+                signUpPage4.setVisibility(View.VISIBLE);
+                break;
+            default:
+                currentSignUpPageNo = 1; //just for testing page change
+        }
+
     }
 }
